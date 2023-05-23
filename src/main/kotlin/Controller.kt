b6b06db6b2dc3d77f52c881dcd77f78f1230f4b6
uhfonso <62177390+uhfonso@users.noreply.github.com>
2023-05-23 09:57:06 +0100

    Started implementing add feature in Editor View

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -5,22 +5,33 @@ import javax.swing.*
 
 fun main() {
     val model = JsonObject()
+    val studentArray = JsonArray()
     Editor(model).open()
 
     model.addElement("uc", JsonString("PA"))
     model.addElement("ects", JsonNumber(6.0))
     model.addElement("data-exame", JsonNull())
+
+    model.addElement("inscritos", studentArray)
+
     val student1 = JsonObject()
-    model.addElement("aluno", student1)
+    studentArray.addElement(student1)
     student1.addElement("numero", JsonNumber(101101))
     student1.addElement("nome", JsonString("Dave Farley"))
     student1.addElement("internacional", JsonBoolean(true))
+
+    val student2 = JsonObject()
+    studentArray.addElement(student2)
+    student2.addElement("numero", JsonNumber(101102))
+    student2.addElement("nome", JsonString("Martin Fowler"))
+    student2.addElement("internacional", JsonBoolean(true))
 
     //GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames().forEach { print(it) }
 }
 
 class Editor(private val model: JsonObject) {
     private val frame = JFrame("JSON Object Editor").apply {
+
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         layout = GridLayout(0, 2)
         size = Dimension(600, 600)
@@ -31,9 +42,11 @@ class Editor(private val model: JsonObject) {
         val textView = JsonTextView(model)
         right.add(textView)
 
+        var editorView : JsonEditorView = JsonEditorView(model, textView)
+
         val left = JPanel()
         left.layout = GridLayout()
-        val scrollPane = JScrollPane(JsonEditorView(model, textView)).apply {
+        val scrollPane = JScrollPane(editorView).apply {
             horizontalScrollBarPolicy = JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
             verticalScrollBarPolicy = JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
         }
@@ -41,6 +54,13 @@ class Editor(private val model: JsonObject) {
 
         add(left)
         add(right)
+
+        editorView.addObserver(object : JsonEditorViewObserver {
+            override fun widgetAdded(element: JsonElement) {
+                model.addElement("test", element)
+            }
+
+        })
     }
 
     fun open() {
